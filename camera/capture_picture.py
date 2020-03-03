dfd885a95cbf45f77eef7dae369cecf5381715c5
jerryzhao173985 <44931279+jerryzhao173985@users.noreply.github.com>
2020-03-03 15:24:05 +0000

    Add files via upload

---
+++ camera/capture_picture.py
@@ -0,0 +1,19 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+
+    # Display the resulting frame
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        cv2.imwrite('save_picture.jpg',frame)
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
